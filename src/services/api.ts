b0dac9364b5d36b593e6b9f0b8b107af60b639e1
Senott <senott@yahoo.com>
2020-10-13 10:59:31 -0300

    Initial configuration.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    Platform.OS === 'ios' ? 'http://localhost:3333' : 'http://10.0.2.2:3333',
 });
 
 export default api;
